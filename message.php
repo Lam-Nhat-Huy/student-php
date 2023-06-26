370fd6c8699635db0101a3e2528a1701bd7c26ff
huydev <lamnhathuy0393418721@gmail.com>
2023-06-26 15:56:39 +0700

    student management system

--- message.php
+++ message.php
@@ -2,7 +2,7 @@
 if (isset($_SESSION['message'])) :
 ?>
     <div class="alert alert-success alert-dismissible fade show" role="alert">
-        <strong>Hey!</strong> <?= $_SESSION['message']; ?>
+        <strong>Hey!</strong><?= $_SESSION['message']; ?>
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
 <?php
